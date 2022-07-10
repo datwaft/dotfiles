6ae236a83007c34e91e5b5bb385cebdbf2ebaebe
datwaft <datwaftx@gmail.com>
2022-07-09 23:51:14 -0600

    fix(scripts): support no parameters for change-background.sh

--- .scripts/change-background.sh
+++ .scripts/change-background.sh
@@ -1,11 +1,26 @@
-if [ "$1" == "dark" ]; then
+if [ -z "$1" ]; then
+  defaults read -g AppleInterfaceStyle &> /dev/null
+  if [[ $? -eq 0 ]]; then
+    mode="dark"
+  else
+    mode="light"
+  fi
+else
+  mode=$1
+fi
+
+if [ "$mode" == "dark" ]; then
   tmux source-file ~/.config/tmux/theme_dark.conf
-  for pipe in /tmp/nvim*.pipe; do
-    nvim --server $pipe --remote-send ':set background=dark<cr>'
-  done
+  if compgen -G '/tmp/nvim_*.pipe' > /dev/null; then
+    for pipe in /tmp/nvim_*.pipe; do
+      /usr/local/bin/nvim --server $pipe --remote-send ':set background=dark<cr>'
+    done
+  fi
 else
   tmux source-file ~/.config/tmux/theme_light.conf
-  for pipe in /tmp/nvim*.pipe; do
-    nvim --server $pipe --remote-send ':set background=light<cr>'
-  done
+  if compgen -G '/tmp/nvim_*.pipe' > /dev/null; then
+    for pipe in /tmp/nvim_*.pipe; do
+      /usr/local/bin/nvim --server $pipe --remote-send ':set background=light<cr>'
+    done
+  fi
 fi

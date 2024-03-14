a92c6fd6a17cf7d1abd3b8f3cdb7789a3eb5f35e
datwaft <datwaftx@gmail.com>
2024-03-14 12:12:47 -0600

    feat(tmux): add color to choose-session border label

--- .config/tmux/choose-session.sh
+++ .config/tmux/choose-session.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 sesh connect "$(sesh list -t | fzf-tmux -p 55%,60% \
-  --no-sort --border-label ' sesh ' --prompt '   ' \
+  --no-sort --border-label [1m[3m' Choose session '[22m[23m --prompt '   ' \
   --header [34m'  <C-a> '[1m'all'[22m' <C-t> '[1m'tmux'[22m' <C-x> '[1m'zoxide'[22m' <C-d> '[1m'tmux kill'[22m' <C-f> '[1m'find'[22m[39m \
   --bind 'tab:down,btab:up' \
   --bind 'ctrl-a:change-prompt(   )+reload(sesh list -tz)' \

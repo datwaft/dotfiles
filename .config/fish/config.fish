bdf56a92ea60978feefdf988fef10adcc2cf5b41
datwaft <datwaftx@gmail.com>
2024-03-19 16:34:59 -0600

    feat(fish): use Catppuccin Mocha colorscheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,13 @@ if status is-interactive
   # ===================
   # Theme configuration
   # ===================
-    # Set theme to Rosé Pine
-    fish_config theme choose 'Rosé Pine'
-    # Set theme to Rosé Pine for fzf
-    set -gx FZF_DEFAULT_OPTS "
-    --color=fg:#908caa,bg:#191724,hl:#ebbcba
-    --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
-    --color=border:#403d52,header:#31748f,gutter:#191724
-    --color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
-    --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+    # Set theme to Catppuccin Mocha
+    fish_config theme choose 'Catppuccin Mocha'
+    # Set theme to Catppuccin Mocha for fzf
+    set -gx FZF_DEFAULT_OPTS "\
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
     # Set theme to base16 for bat
     set -gx BAT_THEME base16
   # ====================

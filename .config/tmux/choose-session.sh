ea2d31ee0a3078e7de62356097321809af9de870
datwaft <datwaftx@gmail.com>
2024-04-01 14:09:26 -0600

    feat(tmux): enhance even more `choose-session`

--- .config/tmux/choose-session.sh
+++ .config/tmux/choose-session.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
+prompt_all=' '
+prompt_tmux=' '
+prompt_zoxide='󰤏 '
+prompt_find='󱦞 '
+
 # shellcheck disable=SC2016
 sesh connect "$(sesh list -t | fzf-tmux -p 55%,60% \
   --color=bg+:#1e1e2e,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-  --reverse --no-scrollbar --pointer '➤ ' --info=inline-right \
-  --no-sort --border-label [1m[3m' Choose Session '[22m[23m --prompt '   ' \
-  --header [2m'   ^a all ∣ ^t tmux ∣ ^x zoxide ∣ ^d kill ∣ ^f find'[22m \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#cba6f7 \
+  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#cba6f7 \
+  --reverse --no-scrollbar --pointer='➤ ' --info=default \
+  --no-sort --border-label [1m[3m' Choose Session '[22m[23m --prompt "${prompt_tmux}  " \
+  --header [2m'^a all ∣ ^t tmux ∣ ^x zoxide ∣ ^d kill ∣ ^f find'[22m \
   --preview 'tmux capture-pane -e -p -t {} 2> /dev/null || \
              eza -I "Icon" --color=always -1 (string replace "~" $HOME {})' \
   --preview-label [1m[3m' Session Preview '[22m[23m \
   --preview-window 'right,<50(down)' \
   --bind 'tab:down,btab:up' \
-  --bind 'ctrl-a:change-prompt(   )+reload(sesh list -tz)' \
-  --bind 'ctrl-t:change-prompt(   )+reload(sesh list -t)' \
-  --bind 'ctrl-x:change-prompt(󱃪   )+reload(sesh list -z)' \
-  --bind 'ctrl-f:change-prompt(   )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-  --bind 'ctrl-d:execute(tmux kill-session -t {})+change-prompt(   )+reload(sesh list -t)')"
+  --bind "ctrl-a:change-prompt(${prompt_all}  )+reload(sesh list -tz)" \
+  --bind "ctrl-t:change-prompt(${prompt_tmux}  )+reload(sesh list -t)" \
+  --bind "ctrl-x:change-prompt(${prompt_zoxide}  )+reload(sesh list -z)" \
+  --bind "ctrl-f:change-prompt(${prompt_find}  )+reload(fd -H -d 2 -t d -E .Trash . ~)" \
+  --bind "ctrl-d:execute(tmux kill-session -t {})+change-prompt(${prompt_tmux}  )+reload(sesh list -t)")"

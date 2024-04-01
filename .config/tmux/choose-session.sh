a736185016434c06dc77e87e55a72574714c5341
datwaft <datwaftx@gmail.com>
2024-03-31 23:06:12 -0600

    feat(tmux): enhance choose-session script

--- .config/tmux/choose-session.sh
+++ .config/tmux/choose-session.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 sesh connect "$(sesh list -t | fzf-tmux -p 55%,60% \
-  --no-sort --border-label [1m[3m' Choose session '[22m[23m --prompt '   ' \
-  --header [34m'  <C-a> '[1m'all'[22m' <C-t> '[1m'tmux'[22m' <C-x> '[1m'zoxide'[22m' <C-d> '[1m'tmux kill'[22m' <C-f> '[1m'find'[22m[39m \
+  --reverse --info=inline --no-scrollbar --pointer '➤ ' \
+  --no-sort --border-label [1m[3m' Choose Session '[22m[23m --prompt '   ' \
+  --header [34m'󰌌  ''<C-a> '[1m'all |'[22m' <C-t> '[1m'tmux |'[22m' <C-x> '[1m'zoxide |'[22m' <C-d> '[1m'tmux kill |'[22m' <C-f> '[1m'find'[22m[39m \
+  --preview 'tmux capture-pane -e -p -t {}' --preview-label [1m[3m' Session Preview '[22m[23m \
+  --preview-window 'right,<50(down)' \
   --bind 'tab:down,btab:up' \
   --bind 'ctrl-a:change-prompt(   )+reload(sesh list -tz)' \
   --bind 'ctrl-t:change-prompt(   )+reload(sesh list -t)' \

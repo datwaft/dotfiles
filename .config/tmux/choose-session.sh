1f9bbdee1d2edd025c84a7205e14a7ce61c3235b
datwaft <datwaftx@gmail.com>
2024-03-31 23:28:51 -0600

    feat(tmux): add fallback to eza for choose-session

--- .config/tmux/choose-session.sh
+++ .config/tmux/choose-session.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+# shellcheck disable=SC2016
 sesh connect "$(sesh list -t | fzf-tmux -p 55%,60% \
   --reverse --info=inline --no-scrollbar --pointer '➤ ' \
   --no-sort --border-label [1m[3m' Choose Session '[22m[23m --prompt '   ' \
   --header [34m'󰌌  ''<C-a> '[1m'all |'[22m' <C-t> '[1m'tmux |'[22m' <C-x> '[1m'zoxide |'[22m' <C-d> '[1m'tmux kill |'[22m' <C-f> '[1m'find'[22m[39m \
-  --preview 'tmux capture-pane -e -p -t {}' --preview-label [1m[3m' Session Preview '[22m[23m \
+  --preview 'tmux capture-pane -e -p -t {} 2> /dev/null || \
+             eza -I "Icon" --color=always -1 (string replace "~" $HOME {})' \
+  --preview-label [1m[3m' Session Preview '[22m[23m \
   --preview-window 'right,<50(down)' \
   --bind 'tab:down,btab:up' \
   --bind 'ctrl-a:change-prompt(   )+reload(sesh list -tz)' \

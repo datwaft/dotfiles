762432d92b305182afb6c04db0d6caadbdf240d1
datwaft <datwaftx@gmail.com>
2024-04-04 15:59:05 -0600

    feat(tmux/choose-session): remove info

--- .config/tmux/choose-session.sh
+++ .config/tmux/choose-session.sh
@@ -8,9 +8,9 @@ prompt_find='󱦞 '
 # shellcheck disable=SC2016
 sesh connect "$(sesh list -t | fzf-tmux -p 55%,60% \
   --color=bg+:#1e1e2e,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#cba6f7 \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#1e1e2e,pointer:#cba6f7 \
   --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#cba6f7 \
-  --reverse --no-scrollbar --pointer='➤ ' --info=default --no-sort --border=sharp \
+  --reverse --no-scrollbar --pointer='➤ ' --info=hidden --no-sort --border=sharp \
   --prompt="${prompt_tmux}  " \
   --header=[2m'^a all ∣ ^t tmux ∣ ^x zoxide ∣ ^d kill ∣ ^f find'[22m \
   --preview='tmux capture-pane -e -p -t {} 2> /dev/null || \

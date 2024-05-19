6cce62940cb7e2fd9e975ca07635acff574219a3
datwaft <datwaftx@gmail.com>
2024-05-19 16:44:49 -0600

    feat(tmux): enhance `choose-session` script
    * We are using a variable for each command, to avoid having to repeat them
    
    * We are enhancing the preview to always show only sessions and expand `~` properly

--- .config/tmux/choose-session.sh
+++ .config/tmux/choose-session.sh
@@ -1,24 +1,30 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 prompt_all='^a'
 prompt_tmux='^t'
 prompt_zoxide='^x'
-prompt_find='^f'
 
-# shellcheck disable=SC2016
-sesh connect "$(sesh list -t | fzf-tmux -p 55%,60% \
+command_all=(sesh list --zoxide --tmux)
+command_tmux=(sesh list --tmux)
+command_zoxide=(sesh list --zoxide)
+
+sesh connect "$("${command_tmux[@]}" | fzf-tmux -p 55%,60% \
   --color=bg+:-1,bg:-1,spinner:#f5e0dc,hl:#f38ba8,gutter:-1 \
   --color=fg:#cdd6f4,header:#f38ba8,info:-1,pointer:#cba6f7 \
   --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#cba6f7 \
   --reverse --no-scrollbar --pointer='➤ ' --info=hidden --no-sort --border=sharp \
   --prompt="${prompt_tmux}  " \
-  --header=[2m'^a all ∣ ^t tmux ∣ ^x zoxide ∣ ^d kill ∣ ^f find'[22m \
-  --preview='tmux capture-pane -e -p -t {} 2> /dev/null || \
-             eza -I "Icon" --color=always -1 $(eval echo {})' \
+  --header=[2m'^a all ∣ ^t tmux ∣ ^x zoxide ∣ ^d kill'[22m \
+  --preview=$'
+    f() {
+      set -- $(eval echo "$@")
+      [ $# -eq 0 ] || tmux capture-pane -e -p -t "=$@:" 2> /dev/null \
+                   || eza -I $\'Icon\r\' --color=always -1 "$@"
+    }; f {}
+  ' \
   --preview-window='right,border-sharp,<50(down)' \
   --bind='tab:down,btab:up' \
-  --bind="ctrl-a:change-prompt(${prompt_all}  )+reload(sesh list -tz)" \
-  --bind="ctrl-t:change-prompt(${prompt_tmux}  )+reload(sesh list -t)" \
-  --bind="ctrl-x:change-prompt(${prompt_zoxide}  )+reload(sesh list -z)" \
-  --bind="ctrl-f:change-prompt(${prompt_find}  )+reload(fd -H -d 2 -t d -E .Trash . ~)" \
-  --bind="ctrl-d:execute(tmux kill-session -t {})+change-prompt(${prompt_tmux}  )+reload(sesh list -t)")"
+  --bind="ctrl-a:change-prompt(${prompt_all}  )+reload(${command_all[*]})" \
+  --bind="ctrl-t:change-prompt(${prompt_tmux}  )+reload(${command_tmux[*]})" \
+  --bind="ctrl-x:change-prompt(${prompt_zoxide}  )+reload(${command_zoxide[*]})" \
+  --bind="ctrl-d:execute(tmux kill-session -t {})+change-prompt(${prompt_tmux}  )+reload(${command_tmux[*]})")"

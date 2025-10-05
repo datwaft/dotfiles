4c7665e0179d529f6ac69788d3c79e4446cd4bf9
datwaft <datwaftx@gmail.com>
2025-10-05 01:43:29 -0600

    feat(ipython): only load icat when installed

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,3 +1,9 @@
+from importlib import metadata
+
 c.TerminalInteractiveShell.true_color = True
-c.InteractiveShellApp.extensions = ["autoreload"]
-c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+try:
+    metadata.version("ipython-icat")
+    c.InteractiveShellApp.extensions = ["autoreload", "icat"]
+    c.InteractiveShellApp.exec_lines = ["%autoreload 2", "%plt_icat"]
+except metadata.PackageNotFoundError:
+    pass

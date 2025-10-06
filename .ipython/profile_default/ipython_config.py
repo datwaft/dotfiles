ed13cd1e920fed8ac4b6d7231ae005ac7bfc788a
datwaft <datwaftx@gmail.com>
2025-10-06 14:03:52 -0600

    feat(python): add `euporie`

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,9 +1,3 @@
-from importlib import metadata
-
 c.TerminalInteractiveShell.true_color = True
-try:
-    metadata.version("ipython-icat")
-    c.InteractiveShellApp.extensions = ["autoreload", "icat"]
-    c.InteractiveShellApp.exec_lines = ["%autoreload 2", "%plt_icat"]
-except metadata.PackageNotFoundError:
-    pass
+c.InteractiveShellApp.extensions = ["autoreload"]
+c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
